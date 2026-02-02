55fd88b492fcf8f5a795d2cd70826bb9b80dc74d
unknown <ralc.0595@gmail.com>
2026-02-02 12:26:41 +0100

    Conf env vars for DB

--- smartBrainzApi/server.js
+++ smartBrainzApi/server.js
@@ -9,11 +9,11 @@ dotenv.config();
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    port: 5432,
-    user: 'postgres',
-    password: 'test',
-    database: 'smart-brain',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   },
 });
 
