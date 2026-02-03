1c438bd33e127a62ee504844197c49fba4dbbc71
unknown <ralc.0595@gmail.com>
2026-02-03 12:49:41 +0100

    ENV ADDED

--- smartBrainzApi/server.js
+++ smartBrainzApi/server.js
@@ -6,16 +6,23 @@ import * as image from './controllers/image.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//   },
+// });
+
 const db = knex({
   client: 'pg',
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  },
+  connection: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
+
 
 const app = express();
 app.use(express.json());
